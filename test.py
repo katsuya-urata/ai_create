4583a512f7ff4db7c0c5c303b4deb0184d26c866
浦田勝也 <109472240+katsuya-urata@users.noreply.github.com>
2024-05-06 12:16:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMbsxnYvnucMwtHGwGFoB1K",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
